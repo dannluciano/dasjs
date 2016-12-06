d8205409231efa049d8e5b0fce2b1f7cb81be312
Dann Luciano <dannluciano@gmail.com>
2016-12-06 09:44:44 -0300

    Refactor: Fix Indentation and Typos

--- src/user.js
+++ src/user.js
@@ -1,22 +1,19 @@
 import db from './database'
 
 export default {
-    get: (username) => {
-        return db.one('SELECT * FROM users WHERE username = $1 LIMIT 1', [username])
-    },
-    getAll: () => {
-        return db.any('SELECT id, username, email, admin FROM users ORDER BY admin DESC, username ASC')
-    },
-    update: (username, isAdmin = false) => {
-        return db.none('UPDATE users SET admin=$1 WHERE username = $2', [isAdmin, username])
-    },
-    insert: async (user) => {
-        return db.one('INSERT INTO users (username, email, password) VALUES($[username], $[email], $[password]) returning id', user)
-    },
-    destroy: (user) => {
-        db.none('DELETE FROM users WHERE id=$1', user.id)
-    }
-
-
-
+  get: (username) => {
+    return db.one('SELECT * FROM users WHERE username = $1 LIMIT 1', [username])
+  },
+  getAll: () => {
+    return db.any('SELECT id, username, email, admin FROM users ORDER BY admin DESC, username ASC')
+  },
+  update: (username, isAdmin = false) => {
+    return db.none('UPDATE users SET admin=$1 WHERE username = $2', [isAdmin, username])
+  },
+  insert: (user) => {
+    return db.one('INSERT INTO users (username, email, password) VALUES($[username], $[email], $[password]) returning id', user)
+  },
+  destroy: (user) => {
+    return db.none('DELETE FROM users WHERE id=$1', user.id)
+  }
 }
