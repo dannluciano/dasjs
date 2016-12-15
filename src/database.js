a48c50f81133926941f941068c73ef9467c31157
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-12-15 15:38:01 +0000

    Update database.js
    this code won't work. for the application_name you should pass it into the connection string/config, and for other things you can override the connect event, as shown, so when it is a fresh connection - re-configure it ;)

--- src/database.js
+++ src/database.js
@@ -1,17 +1,19 @@
 import pg from 'pg-promise'
 import configs from './configs'
 
-const pgp = pg()
-const db = pgp(configs.DATABASE_URL)
-
-async function init (db) {
-  try {
-    await db.none(`SET application_name TO 'web'`)
-    await db.none(`SET statement_timeout TO '30s'`)
-  } catch (e) {
-    console.error(e)
+var pgpOptions = {
+  connect: function(client, dc, isFresh) {
+    // re-configuring every fresh connection:
+    if(isFresh) {
+      client.query(`SET statement_timeout TO '30s'`);
+    }
   }
-}
-init(db)
+};
+
+const pgp = pg(pgpOptions)
+
+// TODO: make sure within the url to pass in parameter application name;
+// see: https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax
+const db = pgp(configs.DATABASE_URL)
 
 export default db
