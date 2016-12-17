8726ea92f189412b4bf3731b46162a93ba9be111
Dann Luciano <dannluciano@gmail.com>
2016-12-17 13:24:10 -0300

    Enable SSL in Postgres Conection

--- src/configs.js
+++ src/configs.js
@@ -1,11 +1,14 @@
-const dbConfig = {
+const dbc = {
   host: process.env['PGHOST'] || 'localhost',
   port: process.env['PGPORT'] || 5432,
   database: process.env['PGDATABASE'] || 'dasdb',
   user: process.env['PGUSER'] || 'postgres',
-  password: process.env['PGPASSWORD'] || ''
+  password: process.env['PGPASSWORD'] || '',
+  app_name: process.env['DYNO'] || 'dasweb',
+  ssl: true
 }
-const dbUrl = `${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`
+
+const dbUrl = `${dbc.user}:${dbc.password}@${dbc.host}:${dbc.port}/${dbc.database}?ssl=${dbc.ssl}&application_name=${dbc.app_name}`
 
 const config = {
   ENCRYPT_KEY: process.env['ENC_KEY'] || 'NGjDUsCMqpYg5A77Utv2zG7RONdG3Lmn9v85o26l0pM=',
--- src/database.js
+++ src/database.js
@@ -2,19 +2,15 @@ import pg from 'pg-promise'
 import configs from './configs'
 
 var pgpOptions = {
-  // API: http://vitaly-t.github.io/pg-promise/global.html#event:connect
-  connect: function(client, dc, isFresh) {
-    // re-configuring every fresh connection:
-    if(isFresh) {
-      client.query(`SET statement_timeout TO '30s'`);
+  connect: function (client, dc, isFresh) {
+    if (isFresh) {
+      client.query(`SET statement_timeout TO '30s'`)
     }
   }
-};
+}
 
 const pgp = pg(pgpOptions)
 
-// TODO: make sure within the url to pass in parameter application_name;
-// see: https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax
 const db = pgp(configs.DATABASE_URL)
 
 export default db
