830a9d098d94caa38c678fefd25ff3c96a6937bf
Dann Luciano <dannluciano@gmail.com>
2017-10-08 00:15:23 -0300

    Heroku

--- src/configs.js
+++ src/configs.js
@@ -8,9 +8,7 @@ const dbc = {
   ssl: true
 }
 
-const dbUrl = (process.env['NODE_ENV'] === 'production')
-  ? 'postgres://ybuwlseuvibaro:cdbc9042e2d9f0ea1e689a6b3d7178692ff97de2f995e8940278300a578372bd@ec2-54-235-80-137.compute-1.amazonaws.com:5432/d8jlbi055i5ace'
-  : `${dbc.user}:${dbc.password}@${dbc.host}:${dbc.port}/${dbc.database}?ssl=${dbc.ssl}&application_name=${dbc.app_name}`
+const dbUrl = `${dbc.user}:${dbc.password}@${dbc.host}:${dbc.port}/${dbc.database}?ssl=${dbc.ssl}&application_name=${dbc.app_name}`
 
 const config = {
   ENCRYPT_KEY: process.env['ENC_KEY'] || 'NGjDUsCMqpYg5A77Utv2zG7RONdG3Lmn9v85o26l0pM=',
